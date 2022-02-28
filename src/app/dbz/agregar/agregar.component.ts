be90c2431616db3b0a1bf530efff482da192c7c9
Jurgen Gonzalez <jurgen.gonzalezpadilla@outlook.com>
2022-02-28 08:04:39 -0600

    Agrega componentes finales

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { DbzService } from "../dbz.service";
+import { Personaje } from "../interfaces/dbz.interfaces";
+
+@Component({
+    selector: 'app-agregar',
+    templateUrl: 'agregar.component.html',
+})
+export class AgregarComponent {
+
+    @Input() nuevo: Personaje = {
+        nombre: '',
+        poder: 0,
+    }
+
+    constructor( private dbzService: DbzService){ }
+    // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+    agregar(): void {
+        if (this.nuevo.nombre.trim().length === 0) { return; }
+        // this.onNuevoPersonaje.emit(this.nuevo);
+        this.dbzService.agregarPersonaje(this.nuevo);
+
+        this.nuevo = {nombre: '', poder: 0};
+    }
+}
